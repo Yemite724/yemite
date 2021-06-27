cac6c0de5134c1ae6364140faf6cac801882856f
yemite724 <salamiolayemi37@gmail.com>
2021-06-27 23:05:05 +0100

    first commit

---
+++ sidehustleassign.php
@@ -0,0 +1,17 @@
+<?php
+
+function functionRange($start, $end)
+{
+
+for ($start; $start <= $end; $start++) {
+echo "The number is: $start <br>";
+var_dump($start);
+
+}
+}
+
+functionRange(0, 10)
+
+?>
+
+<?php
---
+++ sidehustleassign2.php
@@ -0,0 +1,10 @@
+<?php
+
+function sumArray(int $a, int $b, int $c, int $d) {
+    $f = $a + $b +$c + $d;
+    return $f;
+}
+
+echo ($f = 3 + 5 + 4 + 8);
+
+?>
